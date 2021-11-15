a14d0c551c6680daa4c403f6b9a5f7b2e57f093f
p22jiacono <p22jiacono@fairfieldprep.org>
2021-11-15 10:19:28 -0500

    add lab 4-2

---
+++ lab_4-2.py
@@ -0,0 +1,18 @@
+#Author:JTI 11/15/21
+
+classes = ['math', 'chem', 'english']
+
+classes.append('history')
+print(classes)
+
+print(classes.index('chem'))
+
+classes.sort()
+print(classes)
+
+subjects = classes.copy()
+
+print(subjects)
+
+subjects.reverse()
+print(subjects)
