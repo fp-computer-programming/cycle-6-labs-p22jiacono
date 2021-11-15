153f205b61b6bbab4f981a3081fbd7e8c07c44cc
p22jiacono <p22jiacono@fairfieldprep.org>
2021-11-15 10:19:07 -0500

    add lab 3-1

---
+++ lab_3-1.py
@@ -0,0 +1,16 @@
+#Author:JTI 11/15/21
+
+my_row = ["Rayn", "Luis"]
+print(my_row)
+
+my_row = my_row + ["jack"]
+print(my_row)
+
+my_row2 = my_row
+
+print(my_row2)
+
+my_row2 = ['Rayn', 'Luis', 'jack']
+del my_row2[1]
+print(my_row2)
+    
